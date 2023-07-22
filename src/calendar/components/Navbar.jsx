3ef641bd2b5fecc1bd20288de6c9bd31e5b4e3a2
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-22 01:26:31 -0600

    Login-Register UI, calendar navbar

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+      <span className="navbar-brand m-0">
+        <i className="fas fa-calendar-alt d-inline-block"></i> Joy Marcelle
+      </span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt d-inline-block"></i> Sign out
+      </button>
+    </div>
+  )
+}
