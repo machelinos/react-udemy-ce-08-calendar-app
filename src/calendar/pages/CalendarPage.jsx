2937e3109082cd1824870650d7eac3a6e1c84ca7
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-22 11:50:01 -0600

    BigCalendar initial setup

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,48 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import enUS from 'date-fns/locale/en-US'
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
 import { Navbar } from '../'
+
+const events = [
+  {
+    title: 'Event title',
+    notes: 'Buy cake',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Joy Marcelle',
+    },
+  },
+]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
     </>
   )
 }
