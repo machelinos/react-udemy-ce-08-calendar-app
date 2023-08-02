f59dec918848718f677da86c260d328547337ae2
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-08-02 14:44:58 -0600

    Changing url location after signing in

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -15,9 +15,15 @@ export const AppRouter = () => {
   return (
     <Routes>
       {status === 'not-authenticated' ? (
-        <Route path="/auth/*" element={<LoginPage />} />
+        <>
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
       ) : (
-        <Route path="/*" element={<CalendarPage />} />
+        <>
+          <Route path="/" element={<CalendarPage />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </>
       )}
 
       <Route path="/*" element={<Navigate to="/auth/login" />} />
