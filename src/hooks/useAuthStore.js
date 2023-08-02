3da3cd4f2d490ca30c59a2df4537f54d335cd3dd
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-08-02 14:34:00 -0600

    Keeping user auth status

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,8 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { startLogin, startRegister } from '../store/auth/thunks'
+import { loginUser, logoutUser } from '../store'
+import { calendarApi } from '../api'
 
 export const useAuthStore = () => {
-  const { errorMessage } = useSelector((state) => state.auth)
+  const { errorMessage, status } = useSelector((state) => state.auth)
   const dispatch = useDispatch()
 
   const initLogin = ({ email, password }) => {
@@ -13,8 +15,28 @@ export const useAuthStore = () => {
     dispatch(startRegister({ name, email, password }))
   }
 
+  const checkTokenAuth = async () => {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+      return dispatch(logoutUser())
+    }
+
+    try {
+      const { data } = await calendarApi.get('/auth/renew')
+      localStorage.setItem('token', data.token)
+      localStorage.setItem('token-init-date', new Date().getTime())
+      dispatch(loginUser({ name: data.name, uid: data.uid }))
+    } catch (error) {
+      localStorage.clear()
+      dispatch(logoutUser())
+    }
+  }
+
   return {
     errorMessage,
+    status,
+    checkTokenAuth,
     initLogin,
     initRegister,
   }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../auth'
 import { CalendarPage } from '../calendar'
+import { useAuthStore } from '../hooks'
+import { useEffect } from 'react'
 
 export const AppRouter = () => {
-  const authStatus = 'not-authenticated'
+  const { status, checkTokenAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkTokenAuth()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <Routes>
-      {authStatus === 'not-authenticated' ? (
+      {status === 'not-authenticated' ? (
         <Route path="/auth/*" element={<LoginPage />} />
       ) : (
         <Route path="/*" element={<CalendarPage />} />
